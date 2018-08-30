60b29a2617ffa737b674e8469913449ba4af152a
franco-melandri <franco.melandri@ynap.com>
2018-08-30 13:58:12 +0200

    adding BinaryGap solution.

---
+++ BinaryGap/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinaryGap
+{
+    public class Solution
+    {
+        public int solution(int N) {
+    
+            const int CNT_ZEROES_CURRENT = 0;
+            const int CNT_ZEROES_MAX = 1;
+
+            int startingFrom = (int)Math.Ceiling(System.Math.Log(N, 2));            
+            int[] counters = new[] { 0, 0 }; 
+
+            for (int exp = startingFrom; exp >= 0; exp--) {
+                counters = ((N & (1 << exp )) == 0) ?
+                                 new [] { counters[CNT_ZEROES_CURRENT] + 1, counters[CNT_ZEROES_MAX] } :
+                                 new [] { 0, Math.Max(counters[CNT_ZEROES_CURRENT], counters[CNT_ZEROES_MAX]) };
+            }
+
+            return counters[CNT_ZEROES_MAX];
+        }
+    }
+}
---
+++ BinaryGap/Tests.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+
+namespace BinaryGap
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        Solution sut = new Solution();
+
+        [Test]
+        public void ShouldReturnFor8() {
+            Assert.AreEqual(sut.solution(8), 0);
+        }
+    
+        [Test]
+        public void ShouldReturnFor9() {
+            Assert.AreEqual(sut.solution(9), 2);
+        }
+    
+        [Test]
+        public void ShouldReturnFor32() {
+            Assert.AreEqual(sut.solution(32), 0);
+        }
+    
+        [Test]
+        public void ShouldReturnFor529() {
+            Assert.AreEqual(sut.solution(529), 4);
+        }
+
+        [Test]
+        public void ShouldReturnFor1041() {
+            Assert.AreEqual(sut.solution(1041), 5);
+        }
+    }
+}
