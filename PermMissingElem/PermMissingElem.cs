e2dee41ce9d0d05e373bc679b649ba5892912024
franco-melandri <franco.melandri@ynap.com>
2018-09-03 10:31:59 +0200

    Perm missing done.

--- PermMissingElem/PermMissingElem.cs
+++ PermMissingElem/PermMissingElem.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 namespace PermMissingElem
 {
     public class Solution
     {
         public int solution(int[] A) {
-            return 0;
+            Int64 shouldBe = (Int64)((A.Length + 1) * (A.Length + 2))/2;
+            Int64 sum = A.Sum(item => (Int64)item);
+            return (int)(shouldBe - sum);
         }
     }
 }
--- PermMissingElem/PermMissingElemTests.cs
+++ PermMissingElem/PermMissingElemTests.cs
@@ -5,10 +5,67 @@ namespace PermMissingElem
     [TestFixture]
     public class PermMissingElemTests
     {
+        private Solution sut = new Solution();
+
         [Test]
-        public void ShouldReturnEmptyDetail() 
+        public void ShouldReturnMissingElelemnt1() 
         {
-            Assert.AreEqual(new Solution().solution(new int[] {}), 0);
+            int[] input = new int[] { 2, 3, 1, 5 };            
+            Assert.AreEqual(4, sut.solution(input));
+        }
+
+        [Test]
+        public void ShouldReturnMissingElelemnt2() 
+        {
+            int[] input = new int[] { 2, 3, 1, 5, 6 };            
+            Assert.AreEqual(4, sut.solution(input));
+        }
+    
+        [Test]
+        public void ShouldReturnMissingElelemnt3() 
+        {
+            int[] input = new int[] { 2, 4, 1, 5 };            
+            Assert.AreEqual(3, sut.solution(input));
+        }
+
+        [Test]
+        public void ShouldReturnMissingElelemnt4() 
+        {
+            int[] input = new int[] { 2, 4, 1, 5, 6 };            
+            Assert.AreEqual(3, sut.solution(input));
+        }
+
+        [Test]
+        public void ShouldReturnMissingElelemnt5() 
+        {
+            const int NUM_ITEMS = 100000;
+            int[] input = new int[NUM_ITEMS];           
+            for(int i = 0; i < NUM_ITEMS; i++)
+                input[i] = i + 1;
+            input[0] = NUM_ITEMS + 1;
+            Assert.AreEqual(1, sut.solution(input));
+        }
+
+        [Test]
+        public void ShouldReturnMissingElelemnt6() 
+        {
+            const int NUM_ITEMS = 100000;
+            int[] input = new int[NUM_ITEMS];           
+            for(int i = 0; i < NUM_ITEMS; i++)
+                input[i] = i + 1;
+            input[90] = NUM_ITEMS + 1;
+            Assert.AreEqual(91, sut.solution(input));
+        }
+    
+        [Test]
+        public void ShouldReturnMissingElelemnt7() 
+        {
+            const int NUM_ITEMS = 100000;
+            int[] input = new int[NUM_ITEMS];           
+            for(int i = 0; i < NUM_ITEMS; i++)
+                input[i] = i + 1;
+            input[NUM_ITEMS - 1] = NUM_ITEMS + 1;
+            Assert.AreEqual(NUM_ITEMS, sut.solution(input));
         }
     }
 }
