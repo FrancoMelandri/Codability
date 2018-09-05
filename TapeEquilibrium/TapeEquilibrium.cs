a574f5f133fabaf63f28df1b50d618f7a2223ba3
franco-melandri <franco.melandri@ynap.com>
2018-09-05 13:28:19 +0100

    improving tests and back to int

--- TapeEquilibrium/TapeEquilibrium.cs
+++ TapeEquilibrium/TapeEquilibrium.cs
@@ -7,13 +7,13 @@ namespace TapeEquilibrium
     {
         public int solution(int[] A) {
             int minDiff = int.MaxValue;
-            Int64 sumLeft = 0;
-            Int64 sumRigth = A.Sum(i => (Int64)i);
+            int sumLeft = 0;
+            int sumRigth = A.Sum();
 
             for (int i = 1; i < A.Length; i++) {
                 sumLeft += A[i - 1];
                 sumRigth -= A[i - 1];
-                minDiff = (int)Math.Min( Math.Abs(sumLeft - sumRigth), minDiff);
+                minDiff = Math.Min(Math.Abs(sumLeft - sumRigth), minDiff);
             }
             return minDiff;
         }
--- TapeEquilibrium/TapeEquilibriumTests.cs
+++ TapeEquilibrium/TapeEquilibriumTests.cs
@@ -15,6 +15,13 @@ namespace TapeEquilibrium
         }
 
         [Test]
+        public void ShouldReturnRight1_1() 
+        {
+            int[] input = new int[] {1, 1, 1, 1};
+            Assert.AreEqual(0, sut.solution(input));
+        }
+    
+        [Test]
         public void ShouldReturnRight2() 
         {
             int[] input = new int[] {3, 1};
@@ -33,6 +40,24 @@ namespace TapeEquilibrium
         {
             int[] input = new int[] {-1000, 1000, 0, 4, 3};
             Assert.AreEqual(1, sut.solution(input));
+        }
+        
+        [Test]
+        public void ShouldReturnRight5() 
+        {
+            int[] input = new int[100000];
+            for (int i = 0; i < 100000; i++)
+                input[i] = 1000;
+            Assert.AreEqual(0, sut.solution(input));
+        }
+
+        [Test]
+        public void ShouldReturnRight6() 
+        {
+            int[] input = new int[100000];
+            for (int i = 0; i < 100000; i++)
+                input[i] = -1000;
+            Assert.AreEqual(0, sut.solution(input));
         }
     }
 }
