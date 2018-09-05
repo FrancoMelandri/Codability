61baf2a6b9a8ad88c0977316e5c86bac5e28a44f
franco-melandri <franco.melandri@ynap.com>
2018-09-05 15:20:59 +0100

    improving code.

--- PermCheck/PermCheck.cs
+++ PermCheck/PermCheck.cs
@@ -5,7 +5,8 @@ namespace PermCheck
     public class Solution
     {
         public int solution(int[] A) {
-            Int64 shouldBe = ((Int64)(A.Length) * (Int64)(A.Length + 1))/2;
+            Int64 length = A.Length;
+            Int64 shouldBe = (length++ * length)/2;
             Int64 sum = 0;
             for(int i = 0; i < A.Length; i++)
                 sum += A[i];
