85c667e871db9bc40f06b1af2fbbf0bb1dde52d0
franco-melandri <franco.melandri@ynap.com>
2018-09-08 19:13:10 +0100

    first verions for max counters.

--- MaxCounters/Solution.cs
+++ MaxCounters/Solution.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace MaxCounters
 {
     public class Solution
     {
         public int[] solution(int N, int[] A) {
-            return new int[] {};
+            int[] result = new int[N];
+            int max = 0;
+            for (int i = 0; i < A.Length; i++) {
+                if (A[i] <= N) {
+                    max = Math.Max (++result[A[i] -1], max);
+                }
+                else
+                    result = Enumerable.Repeat(max, N).ToArray();
+            }
+            return result;
         }
     }
 }
--- MaxCounters/Tests.cs
+++ MaxCounters/Tests.cs
@@ -9,7 +9,26 @@ namespace MaxCounters
 
         [Test]
         public void ShouldReturnOk() {
-            Assert.IsEmpty(sut.solution(0,new int[] {}));
+            int[] input = new int[] { 3, 4, 4, 6, 1, 4, 4};
+            int[] result = sut.solution(5, input);
+            
+            Assert.AreEqual(3, result[0]);
+            Assert.AreEqual(2, result[1]);
+            Assert.AreEqual(2, result[2]);
+            Assert.AreEqual(4, result[3]);
+            Assert.AreEqual(2, result[4]);
+        }
+
+        [Test]
+        public void ShouldReturnOk1() {
+            int[] input = new int[] { 3, 4, 4, 6, 1, 4, 6};
+            int[] result = sut.solution(5, input);
+            
+            Assert.AreEqual(3, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(3, result[2]);
+            Assert.AreEqual(3, result[3]);
+            Assert.AreEqual(3, result[4]);
         }
     }
 }
