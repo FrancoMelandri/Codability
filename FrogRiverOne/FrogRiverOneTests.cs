86b338e26d139e497a842341efe175ece152bfd3
franco-melandri <franco.melandri@ynap.com>
2018-09-08 15:57:01 +0100

    formatting code and improving tests.

--- FrogRiverOne/FrogRiverOneTests.cs
+++ FrogRiverOne/FrogRiverOneTests.cs
@@ -29,6 +29,25 @@ namespace FrogRiverOne
         }
 
         [Test]
+        public void ShouldReturnOkHuge() 
+        {
+            int[] input = new int [100000];
+            for (int i = 0; i < 100000; i++)
+                input[i] = i + 1;
+            Assert.AreEqual(99999, sut.solution(100000, input));
+        }
+    
+        [Test]
+        public void ShouldReturnOkHugeLast() 
+        {
+            int[] input = new int [100000];
+            for (int i = 1; i < 99999; i++)
+                input[i] = i + 1;
+            input[99999] = 1;
+            Assert.AreEqual(99999, sut.solution(100000, input));
+        }
+    
+        [Test]
         public void ShouldReturnKo() 
         {
             int[] input = new int [] { 1, 3, 1, 4, 2, 3, 5, 4 };
