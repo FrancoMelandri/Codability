8db8539885979d8da3e0fb1371dacbc2014a9756
franco-melandri <franco.melandri@ynap.com>
2018-09-08 15:48:29 +0100

    first from river one version with some tests

--- FrogRiverOne/FrogRiverOne.cs
+++ FrogRiverOne/FrogRiverOne.cs
@@ -5,7 +5,18 @@ namespace FrogRiverOne
     public class Solution
     {
         public int solution(int X, int[] A) {
-            return 0;
+            int currentSteps = X;
+            bool[] steps = new bool[currentSteps + 1];
+
+            for(int i = 0; i < A.Length; i++){
+                if(!steps[A[i]]){
+                    steps[A[i]] = true;
+                    currentSteps--;
+                }
+                if(currentSteps == 0) 
+                    return i;
+            }
+            return -1;
         }
     }
 }
--- FrogRiverOne/FrogRiverOneTests.cs
+++ FrogRiverOne/FrogRiverOneTests.cs
@@ -5,10 +5,34 @@ namespace FrogRiverOne
     [TestFixture]
     public class FrogRiverOneTests
     {
+        Solution sut = new Solution();
+
         [Test]
         public void ShouldReturnOk() 
         {
-            Assert.AreEqual(new Solution().solution(0, new int[] {}), 0);
+            int[] input = new int [] { 1, 3, 1, 4, 2, 3, 5, 4 };
+            Assert.AreEqual(6, sut.solution(5, input));
+        }
+
+        [Test]
+        public void ShouldReturnOk1() 
+        {
+            int[] input = new int [] { 1, 2, 3, 4, 5, 3, 5, 4 };
+            Assert.AreEqual(4, sut.solution(5, input));
+        }
+
+        [Test]
+        public void ShouldReturnOk2() 
+        {
+            int[] input = new int [] { 1, 5, 3, 4, 5, 3, 2, 4 };
+            Assert.AreEqual(6, sut.solution(5, input));
+        }
+
+        [Test]
+        public void ShouldReturnKo() 
+        {
+            int[] input = new int [] { 1, 3, 1, 4, 2, 3, 5, 4 };
+            Assert.AreEqual(-1, sut.solution(8, input));
         }
     }
 }
