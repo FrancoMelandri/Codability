a8b77634b3c20e662bf3ee9cc564b3f542df7ddc
franco-melandri <franco.melandri@ynap.com>
2018-09-08 18:34:11 +0100

    refactoring code.

--- FrogRiverOne/FrogRiverOne.cs
+++ FrogRiverOne/FrogRiverOne.cs
@@ -5,17 +5,13 @@ namespace FrogRiverOne
     public class Solution
     {
         public int solution(int X, int[] A) {
-            int currentSteps = X;
-            bool[] steps = new bool[currentSteps + 1];
-
+            bool[] route = new bool[X + 1];
             for (int i = 0; i < A.Length; i++) {
-                if (!steps[A[i]]) {
-                    steps[A[i]] = true;
-                    currentSteps--;
+                int step = A[i];
+                if (!route[step]) {
+                    route[step] = true;
+                    if (--X == 0) return i;
                 }
-
-                if (currentSteps == 0) 
-                    return i;
             }
             return -1;
         }
