54fa426941259994ce89ec66fc0b4002433ffe1f
franco-melandri <franco.melandri@ynap.com>
2018-09-09 07:33:21 +0100

    adding test and code for MissingInteger

--- MissingInteger/Solution.cs
+++ MissingInteger/Solution.cs
@@ -5,7 +5,21 @@ namespace MissingInteger
     public class Solution
     {
         public int solution(int[] A) {
-            return 0;
+            const int MAX_ELEM = 1000000;
+            const int NOT_FOUND = 1;
+            System.Collections.Generic.List<int> map = new System.Collections.Generic.List<int>();
+
+            for (int i = 0; i < A.Length; i++) {
+                if (A[i] > 0) {
+                    map.Add(A[i]);
+                }
+            }
+
+            for (int i = 1; i <= MAX_ELEM; i++) {
+                if (!map.Contains(i)) return i;
+            }
+
+            return NOT_FOUND;
         }
     }
 }
--- MissingInteger/Tests.cs
+++ MissingInteger/Tests.cs
@@ -9,10 +9,34 @@ namespace MissingInteger
 
         [Test]
         public void ShouldReturnOk() {
-            int[] input = new int[] { };
+            int[] input = new int[] { 1, 3, 6, 4, 1, 2 };
             int result = sut.solution(input);
         
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void ShouldReturnOk1() {
+            int[] input = new int[] { 1, 2, 3 };
+            int result = sut.solution(input);
+        
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void ShouldReturnOk2() {
+            int[] input = new int[] { 2, 3, 6, 4, 2, 2 };
+            int result = sut.solution(input);
+        
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void ShouldReturnOk3() {
+            int[] input = new int[] { 2, 3, 6, 4, 1, 2 };
+            int result = sut.solution(input);
+        
+            Assert.AreEqual(5, result);
         }
     }
 }
